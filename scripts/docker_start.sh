7b57596960eb660a1dbc8947505b92cb01a33d94
greatsheep <pratikpradhane1@gmail.com>
2026-03-10 15:41:02 +0530

    maps pushed

---
+++ scripts/docker_start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo docker run -it --rm \
+  --name crawler-container \
+  --privileged \
+  --network host \
+  --device /dev/ttyUSB_LIDAR \
+  --device /dev/ttyUSB_SERIAL \
+  -v /dev/ttyUSB_LIDAR:/dev/ttyUSB_LIDAR \
+  -v /dev/ttyUSB_SERIAL:/dev/ttyUSB_SERIAL \
+  --group-add dialout \
+  -v /home/greatsheep/crawler:/home/ubuntu/ros2_ws/src \
+  crawler:latest
